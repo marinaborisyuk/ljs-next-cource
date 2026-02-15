8e7e34c7483c2ffbdbbb8804f7bae0aac4da1f2e
Marina Borisyuk <marinaborisyuk1@gmail.com>
2026-02-15 19:02:01 +0300

    init: add default project configs & remove useless code

---
+++ sports-equipment-shop/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Sport Equipment Shop",
+  description: "Sport Equipment Shop",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
